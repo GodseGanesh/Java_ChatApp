136023b113530b4afc6634c140fbc978bcda5201
GodseGanesh <ganeshgodse1902@gmail.com>
2025-04-11 15:46:16 +0530

    added proxy to redirects

--- src/config/AxoisHelper.js
+++ src/config/AxoisHelper.js
@@ -1,7 +1,7 @@
-import axios from 'axios';
-export const baseURL = "http://13.51.146.169:8080"
+export const baseURL = process.env.NODE_ENV === 'production'
+  ? "" // Let Netlify proxy it!
+  : "http://13.51.146.169:8080"; // Local dev direct API
+
 export const httpClient = axios.create({
-    baseURL:baseURL ,
-    
-    
-})
+  baseURL: baseURL
+});
