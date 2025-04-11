98b9ba3e58ba3023325015186a9b228f7e7df0e0
GodseGanesh <ganeshgodse1902@gmail.com>
2025-04-11 14:37:30 +0530

    changed the base url

--- src/config/AxoisHelper.js
+++ src/config/AxoisHelper.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-export const baseURL = process.env.REACT_APP_API_BASE_URL || "http://localhost:8080";
+export const baseURL = "http://13.51.146.169:8080"
 export const httpClient = axios.create({
     baseURL:baseURL ,
     
